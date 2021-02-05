917b7226f29fef0f121b2227e43eee0737194a96
Orwa_Watad <theorwa@gmail.com>
2021-02-05 14:20:32 +0200

    timers & temp

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ app.use(cors())
 
 const port = process.env.PORT || 3000
 
+var timers = "comp = 00:00:00,notComp = 00:00:00,heat = 00:00:00,notHeat = 00:00:00";
+var temp = "temp1 = 00.00 C,temp2 = 00.00 C";
+
 app.get('/api', (req, res) => {
   res.status(200).json({api: 'version 1'})
 })
 
 app.get('/data', (req, res) => {
-  res.status(200).json({data: 'comp = 00:00:00\nheat = 00:00:00'})
+  res.status(200).json({timers, temp})
 })
 
 app.listen(port, () => console.log('server started'))
