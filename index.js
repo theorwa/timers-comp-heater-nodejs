b70cfcbce2acdca530f7cc561e417ec45dba5595
Orwa_Watad <theorwa@gmail.com>
2021-02-05 18:41:40 +0200

    fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json())
 
 const port = process.env.PORT || 3000
 
-var timers = "compres: 00:00:00,notComp = 00:00:00,heater = 00:00:00,notHeat = 00:00:00";
+var timers = "compres = 00:00:00,notComp = 00:00:00,heater = 00:00:00,notHeat = 00:00:00";
 var temp = "temp1 = 00.00 C,temp2 = 00.00 C";
 
 app.get('/api', (req, res) => {
