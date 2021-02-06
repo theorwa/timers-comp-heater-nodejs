48abce1ad610ac7b097a342035b462d4f078ed44
Orwa_Watad <theorwa@gmail.com>
2021-02-06 23:33:01 +0200

    upgrade

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-const express = require('express')
-var bodyParser = require('body-parser')
-// const socketIo = require("socket.io");
+const express = require('express');
+const bodyParser = require('body-parser');
+const perf_hooks = require('perf_hooks');
+const helmet = require('helmet');
 const http = require("http");
+require('express-async-errors'); // for exception handler
+
+const requestTime = (req, res, next) => {
+    req.requestTime = perf_hooks.performance.now();
+    next();
+}
 
 const port = process.env.PORT || 3000
 const router = require("./routes");
 
 const app = express();
+app.use(helmet());
+app.use(requestTime);
+app.use(bodyParser.json({type: "*/*"}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(async (err, req, res, next) => {
+    res.status(500).send({status: "error", cause: err.message})
+});
 
 const server = http.createServer(app);
-// const io = socketIo(server); // < Interesting!
-
-// let interval;
-
-// io.on("connection", (socket) => {
-//   console.log("New client connected");
-//   if (interval) {
-//     clearInterval(interval);
-//   }
-//   interval = setInterval(() => getApiAndEmit(socket), 1000);
-//   socket.on("disconnect", () => {
-//     console.log("Client disconnected");
-//     clearInterval(interval);
-//   });
-// });
-
-// const getApiAndEmit = socket => {
-//   // const response = {timers: router.timers, temp: router.temp};
-//   const response = "ORWA123";
-//   // Emitting a new message. Will be consumed by the client
-//   socket.emit("FromAPI", response);
-// };
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
--- routes.js
+++ routes.js
@@ -7,10 +7,6 @@ router.all('*', cors());
 var timers = "compres = 00:00:00,notComp = 00:00:00,heater = 00:00:00,notHeat = 00:00:00";
 var temp = "temp 1 max = 00.00 C,temp 1 now = 00.00 C,temp 1 min = 00.00 C,temp 2 max = 00.00 C,temp 2 now = 00.00 C,temp 2 min = 00.00 C";
 
-router.get('/api', (req, res) => {
-    res.status(200).json({api: 'version 1'})
-})
-
 router.get('/data', (req, res) => {
     res.status(200).json({timers, temp})
 })
@@ -20,11 +16,11 @@ router.post('/data', (req, res) => {
     {
         timers = req.body.timers;
         temp = req.body.temp;
-        res.send("OK");
+        res.status(200).send("Success");
     }
     else
     {
-        res.send("Failed");
+        res.status(400).send("Failed");
     }
 })
 
