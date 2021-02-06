4796d4905a963d45e01119b5ceb3a72269f4e2b8
Orwa_Watad <theorwa@gmail.com>
2021-02-06 17:41:33 +0200

    update

--- routes.js
+++ routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.all('*', cors());
 
 var timers = "compres = 00:00:00,notComp = 00:00:00,heater = 00:00:00,notHeat = 00:00:00";
-var temp = "temp1 max = 00.00 C,temp1 now = 00.00 C,temp1 min = 00.00 C,temp2 max = 00.00 C,temp2 now = 00.00 C,temp2 min = 00.00 C";
+var temp = "temp 1 max = 00.00 C,temp 1 now = 00.00 C,temp 1 min = 00.00 C,temp 2 max = 00.00 C,temp 2 now = 00.00 C,temp 2 min = 00.00 C";
 
 router.get('/api', (req, res) => {
     res.status(200).json({api: 'version 1'})
