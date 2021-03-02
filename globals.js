cc2efb06c41db43efe14f455b70c4a48f1ad3d4f
Orwa_Watad <theorwa@gmail.com>
2021-03-02 21:42:19 +0200

    update server with socket.io

---
+++ globals.js
@@ -0,0 +1,17 @@
+var timers = "compres: = 00:00:00,notComp: = 00:00:00,heater : = 00:00:00,notHeat: = 00:00:00";
+var temp = "temp 1 max: = 00.00 C,temp 1 now: = 00.00 C,temp 1 min: = 00.00 C,temp 2 max: = 00.00 C,temp 2 now: = 00.00 C,temp 2 min: = 00.00 C";
+
+const serversocket = (io) => {
+    if (io) globalio = io;
+    if (globalio && globalio.sockets)
+    {
+        globalio.sockets.emit('data', {timers, temp});
+        console.log("server socket sends data to clients");
+    }
+}
+
+module.exports = {
+    serversocket,
+    timers,
+    temp,
+};
--- routes.js
+++ routes.js
@@ -1,11 +1,9 @@
 const cors = require('cors');
 const express = require("express");
 const router = express.Router();
+const { serversocket, timers, temp } = require("./globals");
 
 router.all('*', cors());
-
-var timers = "compres: = 00:00:00,notComp: = 00:00:00,heater : = 00:00:00,notHeat: = 00:00:00";
-var temp = "temp 1 max: = 00.00 C,temp 1 now: = 00.00 C,temp 1 min: = 00.00 C,temp 2 max: = 00.00 C,temp 2 now: = 00.00 C,temp 2 min: = 00.00 C";
 
 router.get('/data', (req, res) => {
     res.status(200).json({timers, temp})
@@ -13,15 +11,19 @@ router.get('/data', (req, res) => {
 
 router.post('/data', (req, res) => {
     if (req.body && req.body.timers && req.body.temp)
-    {
+    { 
         timers = req.body.timers;
         temp = req.body.temp;
+        serversocket();
         res.status(200).send("Success");
     }
     else
     {
+        serversocket();
         res.status(400).send("Failed");
     }
 })
 
-module.exports = router;
+module.exports = {
+    router,
+};
